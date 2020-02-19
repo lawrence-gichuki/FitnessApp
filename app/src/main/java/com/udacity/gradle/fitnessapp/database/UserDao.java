7443d282fd4bbb079e1d3e96e8d8b0b03fcaf933
Lawrence Gichuki <lawrencegichuki@gmail.com>
2020-02-19 16:57:16 +0300

    Add DateConverter

--- app/src/main/java/com/udacity/gradle/fitnessapp/database/UserDao.java
+++ app/src/main/java/com/udacity/gradle/fitnessapp/database/UserDao.java
@@ -1,8 +1,25 @@
 package com.udacity.gradle.fitnessapp.database;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
 
 @Dao
 public interface UserDao {
+@Query("SELECT * FROM users")
+    List<UserProfile> loadAllTasks();
 
+@Insert
+    void insertUser(UserProfile userProfile);
+
+@Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateUser(UserProfile userProfile);
+
+@Delete
+    void deleteUser(UserProfile userProfile);
 }
