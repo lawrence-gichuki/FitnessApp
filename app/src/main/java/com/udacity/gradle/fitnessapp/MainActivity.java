90acd475e011fbe6b26504d71722a6fdefcbf7c6
Lawrence Gichuki <lawrencegichuki@gmail.com>
2020-02-20 08:42:15 +0300

    Update the Report UI

--- app/src/main/java/com/udacity/gradle/fitnessapp/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/fitnessapp/MainActivity.java
@@ -120,7 +120,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Intent intent = new Intent(this,SettingsActivity.class);
             startActivity(intent);
             return true;
-        }
+        } else
+            if (id == R.id.action_report) {
+                Intent intent = new Intent(this, ReportActivity.class);
+                startActivity(intent);
+                return true;
+            }
 
         return super.onOptionsItemSelected(item);
     }
